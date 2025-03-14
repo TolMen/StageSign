b9c60e2c894f94f74d5faa7fc90ddc1733714fbd
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-14 08:54:28 +0100

    Requête BDD de connexion de l'utilisateur

---
+++ src/model/userModel/userAuthModel.php
@@ -0,0 +1,17 @@
+<?php
+
+// Inclusion de fichier nécessaire
+require_once '../../controller/BDDController/connectBDD.php';
+
+// Classe pour gérer les opérations de connexion
+class UserAuthModel
+{
+
+    // Cette fonction récupère le pseudo des utilisateurs
+    public function getAuthUser(PDO $bdd, $pseudo, $password)
+    {
+        $recupUser = $bdd->prepare('SELECT * FROM user WHERE pseudo = ? AND password = SHA2(?, 256)');
+        $recupUser->execute([$pseudo, $password]);
+        return $recupUser->fetch(PDO::FETCH_ASSOC);
+    }
+}
