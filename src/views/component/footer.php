00ba6ed6cabef1026d5c78bc14482f896e55d909
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-14 08:55:54 +0100

    Ajout des composants de base

---
+++ src/views/component/footer.php
@@ -0,0 +1,12 @@
+<footer class="bg-dark text-light py-4">
+    <div class="container text-center">
+        <p class="mb-0">
+            &copy;
+            <?php
+            date_default_timezone_set("Europe/Paris");
+            echo date("Y");
+            ?>
+            ??? | Tous les droits sont réservés.
+        </p>
+    </div>
+</footer>
---
+++ src/views/component/head.php
@@ -0,0 +1,28 @@
+    <!-- Essential meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="mobile-web-app-capable" content="yes" />
+
+    <!-- SEO information -->
+    <meta name="keywords" content="####">
+    <meta name="description" content="####">
+    <meta name="author" content="#####" />
+
+    <!-- Site icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="#" />
+
+    <!-- Script -->
+    <script src="https://cdn.tiny.cloud/1/997xkqpa1g46b2womha0n37h0fv2o2rd9gk9espe7dmw08rx/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <!-- - External style sheets -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+
+    <!-- Links to JavaScript scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Custom style sheets -->
+    <link rel="stylesheet" href="../../../assets/css/baseStyle.css" />
