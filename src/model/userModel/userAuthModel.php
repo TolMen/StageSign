2238a34691f44d0bc8f143f17893419dd5881064
nolansio <nolanarsac105@gmail.com>
2025-03-21 08:22:34 +0100

    Refactor la gestion de la connexion à la base de données et mise à jour des chemins d'inclusion

--- src/model/userModel/userAuthModel.php
+++ src/model/userModel/userAuthModel.php
@@ -1,7 +1,7 @@
 <?php
 
 // Inclusion de fichier nécessaire
-require_once '../../controller/BDDController/connectBDD.php';
+require_once './src/controller/bddController/connectBDD.php';
 
 // Classe pour gérer les opérations de connexion
 class UserAuthModel
