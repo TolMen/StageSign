c5cab190a0c094d39f69371b810d1e2f19d0c26c
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-21 08:40:27 +0100

    Modification BDD + navbar

--- assets/bdd.sql
+++ assets/bdd.sql
@@ -14,27 +14,26 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
 
--- Table des entreprise
+-- Table des entreprises
 CREATE TABLE IF NOT EXISTS compagnies (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    firts_adress VARCHAR(255) NOT NULL,
-    second_adress VARCHAR(255),
-    postal_code INT NOT NULL,
+    first_address VARCHAR(255) NOT NULL,
+    second_address VARCHAR(255),
+    postal_code VARCHAR(10) NOT NULL,
     city VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL
 ) ENGINE=InnoDB;
-
 
 -- Table des étudiants
 CREATE TABLE IF NOT EXISTS students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     class VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL, 
-    user_id INT NOT NULL
+    user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
@@ -42,38 +41,38 @@ CREATE TABLE IF NOT EXISTS students (
 CREATE TABLE IF NOT EXISTS tutors (
     id INT AUTO_INCREMENT PRIMARY KEY,
     function VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL
-    compagny_id INT NOT NULL
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    FOREIGN KEY (compagny_id) REFERENCES compagnies(id) ON DELETE CASCADE
+    user_id INT NOT NULL,
+    company_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES compagnies(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 -- Table des directions
 CREATE TABLE IF NOT EXISTS directions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL
-    compagny_id INT NOT NULL
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    FOREIGN KEY (compagny_id) REFERENCES compagnies(id) ON DELETE CASCADE
+    user_id INT NOT NULL,
+    company_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (company_id) REFERENCES compagnies(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
 -- Table des administrateurs
 CREATE TABLE IF NOT EXISTS school (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL
+    user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
--- Table des convention
+-- Table des conventions
 CREATE TABLE IF NOT EXISTS convention (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE DEFAULT 
-    etat ENUM("attente", "signe", "archive"),
+    name VARCHAR(255) NOT NULL UNIQUE,
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     place VARCHAR(255),
     subject TEXT NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    etat ENUM('attente', 'signe', 'archive') NOT NULL
 ) ENGINE=InnoDB;
 
 
--- src/views/component/navbar.php
+++ src/views/component/navbar.php
@@ -10,9 +10,16 @@ $current_page = basename($_SERVER['PHP_SELF']);
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php">Accueil</a>
-                </li>
+                <?php 
+                if($current_page == 'loginForm.php') { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" <?php echo ($current_page == 'home.php') ? 'active text-primary' : ''; ?> href="../page/home.php">Accueil</a>
+                    </li>
+                <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" <?php echo ($current_page == 'home.php') ? 'active text-primary' : ''; ?> href="home.php">Accueil</a>
+                    </li>
+                <?php } ?>
                 <li class="nav-item">
                     <a class="nav-link" href="">FAQ</a>
                 </li>
@@ -21,18 +28,14 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <a class="nav-link" href="">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="">Connexion</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="">Inscription</a>
+                    <a class="nav-link" <?php echo ($current_page == 'loginForm.php') ? 'active text-primary' : ''; ?> href="../form/loginForm.php">Connexion</a>
                 </li>
                 <?php if (!empty($_SESSION['id'])) { ?>
                     <li class="nav-item">
                         <a class="nav-link text-danger" href="../../controller/userController/userStop.php" title="Déconnexion">Déconnexion</a>
                     </li>
+                <?php } ?>
             </ul>
         </div>
-    <?php } ?>
     </div>
 </nav>
