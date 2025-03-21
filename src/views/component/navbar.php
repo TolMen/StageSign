2a137a5be1c8c54ec6d054e5612dc4affb8b5ab7
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-21 08:14:49 +0100

    Fix

--- src/views/component/navbar.php
+++ src/views/component/navbar.php
@@ -5,34 +5,34 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom: 1px solid black;">
     <div class="container-fluid">
         <strong><span class="navbar-brand">StageSign</span></strong>
-        <?php if (!empty($_SESSION['id'])) { ?>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="home.php">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">FAQ</a>
-                    </li>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">FAQ</a>
+                </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="">Connexion</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Connexion</a>
+                </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="">Inscription</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Inscription</a>
+                </li>
+                <?php if (!empty($_SESSION['id'])) { ?>
                     <li class="nav-item">
                         <a class="nav-link text-danger" href="../../controller/userController/userStop.php" title="Déconnexion">Déconnexion</a>
                     </li>
-                </ul>
-            </div>
-        <?php } ?>
+            </ul>
+        </div>
+    <?php } ?>
     </div>
 </nav>
